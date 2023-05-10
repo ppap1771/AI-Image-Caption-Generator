038f13d5bbb10dad5beb5d2a2eb361bdd97f1933
ppap1771 <aayusggupta09022003@gmail.com>
2023-05-10 20:39:13 +0530

    update: remove models and added interface

--- app.py
+++ app.py
@@ -1,53 +1,82 @@
-import torch
-import gradio as gr 
-import re 
+import json
+import os, shutil
+import random
+
+
 from PIL import Image
-from transformers import AutoTokenizer, ViTImageProcessor, VisionEncoderDecoderModel
+import jax
+from transformers import FlaxVisionEncoderDecoderModel, ViTFeatureExtractor, AutoTokenizer
+from huggingface_hub import hf_hub_download
 
-device='cpu'
-encoder_checkpoint = "nlpconnect/vit-gpt2-image-captioning"
-decoder_checkpoint = "nlpconnect/vit-gpt2-image-captioning"
-model_checkpoint = "nlpconnect/vit-gpt2-image-captioning"
-feature_extractor = ViTFeatureExtractor.from_pretrained(encoder_checkpoint)
-tokenizer = AutoTokenizer.from_pretrained(decoder_checkpoint)
-model = VisionEncoderDecoderModel.from_pretrained(model_checkpoint).to(device)
 
-def predict(image,max_length=64, num_beams=4):
-  image = image.convert('RGB')
-  image = feature_extractor(image, return_tensors="pt").pixel_values.to(device)
-  clean_text = lambda x: x.replace('<|endoftext|>','').split('\n')[0]
-  caption_ids = model.generate(image, max_length = max_length)[0]
-  caption_text = clean_text(tokenizer.decode(caption_ids))
-  return caption_text 
+# create target model directory
+model_dir = './models/'
+os.makedirs(model_dir, exist_ok=True)
 
-def set_example_image(example: list) -> dict:
-    return gr.Image.update(value=example[0])
+files_to_download = [
+    "config.json",
+    "flax_model.msgpack",
+    "merges.txt",
+    "special_tokens_map.json",
+    "tokenizer.json",
+    "tokenizer_config.json",
+    "vocab.json",
+    "preprocessor_config.json",
+]
 
-css = '''
-h1#title {
-  text-align: center;
-}
-h3#header {
-  text-align: center;
-}
-img#overview {
-  max-width: 800px;
-  max-height: 600px;
-}
-img#style-image {
-  max-width: 1000px;
-  max-height: 600px;
-}
-'''
+# copy files from checkpoint hub:
+for fn in files_to_download:
+    file_path = hf_hub_download("ydshieh/vit-gpt2-coco-en-ckpts", f"ckpt_epoch_3_step_6900/{fn}")
+    shutil.copyfile(file_path, os.path.join(model_dir, fn))
 
-demo = gr.Interface(fn=predict, inputs = "image", outputs = "text")
-with demo:
-  gr.Markdown('''<h1 id="title">Image Caption 🖼️</h1>''')
-  gr.Markdown('''Made by : Shreyas Dixit''')
-  with gr.Column():
-        input = gr.inputs.Image(label="Upload your Image", type = 'pil', optional=True)
-        output = gr.outputs.Textbox(type="auto",label="Captions")
-  btn = gr.Button("Genrate Caption")
-  btn.click(fn=predict, inputs=input, outputs=output)
+model = FlaxVisionEncoderDecoderModel.from_pretrained(model_dir)
+feature_extractor = ViTFeatureExtractor.from_pretrained(model_dir)
+tokenizer = AutoTokenizer.from_pretrained(model_dir)
 
-demo.launch()
+max_length = 16
+num_beams = 4
+gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
+
+
+@jax.jit
+def generate(pixel_values):
+    output_ids = model.generate(pixel_values, **gen_kwargs).sequences
+    return output_ids
+
+
+def predict(image):
+
+    if image.mode != "RGB":
+        image = image.convert(mode="RGB")
+
+    pixel_values = feature_extractor(images=image, return_tensors="np").pixel_values
+
+    output_ids = generate(pixel_values)
+    preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+    preds = [pred.strip() for pred in preds]
+
+    return preds[0]
+
+
+def _compile():
+
+    image_path = 'samples/val_000000039769.jpg'
+    image = Image.open(image_path)
+    predict(image)
+    image.close()
+
+
+_compile()
+
+
+sample_dir = './samples/'
+sample_image_ids = tuple(["None"] + [int(f.replace('COCO_val2017_', '').replace('.jpg', '')) for f in os.listdir(sample_dir) if f.startswith('COCO_val2017_')])
+
+with open(os.path.join(sample_dir, "coco-val2017-img-ids.json"), "r", encoding="UTF-8") as fp:
+    coco_2017_val_image_ids = json.load(fp)
+
+
+def get_random_image_id():
+
+    image_id = random.sample(coco_2017_val_image_ids, k=1)[0]
+    return image_id
